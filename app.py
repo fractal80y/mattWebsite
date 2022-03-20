8ac1c7faf06f11a3a09150c7c998a1d3072a57ef
Matt <mzvirbulis87@gmail.com>
2022-03-20 14:29:30 +1100

    push web server project

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 """test Flask with this"""
 
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+param = request.headers['test']
 
 @app.route('/')
 def hello():
-    return render_template("menu/front")
+    return render_template("menu/front", temp1=param)
+
